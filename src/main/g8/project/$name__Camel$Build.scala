82320f437d149f20401242cc810143c2acd1e12d
Tomoya Kose <mitsuset@gmail.com>
2013-07-26 22:18:00 +0900

    import the ScalaTest as default

--- src/main/g8/project/$name__Camel$Build.scala
+++ src/main/g8/project/$name__Camel$Build.scala
@@ -9,7 +9,10 @@ object $name;format="Camel"$Build extends Build {
             name := "$name$",
             organization := "$organization$",
             version := "$version$",
-            scalaVersion := "$scala_version$"
+            scalaVersion := "$scala_version$",
+            libraryDependencies ++= Seq(
+                "org.scalatest" % "scalatest_2.10" % "1.9.1" % "test"
+            )
         )
     )
 }
